d0ee44124295bd033e9cd727ee903c7b645b4f13
nolancon <conor.nolan@storageos.com>
2021-06-21 15:25:55 +0000

    Remove vendor folder while dev ongoing

--- vendor/github.com/replicatedhq/troubleshoot/pkg/collect/database_shared.go
+++
@@ -1,7 +0,0 @@
-package collect
-
-type DatabaseConnection struct {
-	IsConnected bool   `json:"isConnected"`
-	Error       string `json:"error,omitempty"`
-	Version     string `json:"version,omitempty"`
-}
