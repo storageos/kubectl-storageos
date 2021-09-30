e85527b229a4c56ef5c45abe3d2c9fd516b111ed
nolancon <conor.nolan@storageos.com>
2021-09-30 13:34:51 +0000

    update version message

--- cmd/plugin/cli/version.go
+++ cmd/plugin/cli/version.go
@@ -12,8 +12,8 @@ func VersionCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:          "version",
 		Args:         cobra.MinimumNArgs(0),
-		Short:        "Show the StorageOS version information",
-		Long:         `Show the StorageOS version information`,
+		Short:        "Show kubectl storageos version",
+		Long:         `Show kubectl storageos version`,
 		SilenceUsage: true,
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Printf("%s\n", Version)
