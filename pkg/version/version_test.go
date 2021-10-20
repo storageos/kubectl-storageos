05e0cad4ed1edf9d4aa62c2abc27b8426de5618d
Richard Kovacs <kovacsricsi@gmail.com>
2021-10-20 14:17:14 +0200

    Fix version check for postfixes (#62)
    * Fix version check for postfixes
    
    * Enable supported version parallelism

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -11,6 +11,10 @@ func TestCleanupVersion(t *testing.T) {
 			input:    "v21.42.43",
 			expected: "v21.42.43",
 		},
+		"already cleaned versionless": {
+			input:    "21.42.43",
+			expected: "21.42.43",
+		},
 		"has postfix": {
 			input:    "v21.42.43-alpha1",
 			expected: "v21.42.43",
@@ -27,6 +31,52 @@ func TestCleanupVersion(t *testing.T) {
 
 			if tt.expected != actual {
 				t.Errorf("cleaned version doesn't match: %s != %s", tt.expected, actual)
+			}
+		})
+	}
+}
+
+func TestIsSupported(t *testing.T) {
+	tests := map[string]struct {
+		haveVersion string
+		wantVersion string
+		expected    bool
+	}{
+		"less than": {
+			haveVersion: "1.17.3",
+			wantVersion: "1.18.0",
+		},
+		"greater than": {
+			haveVersion: "1.18.3",
+			wantVersion: "1.18.0",
+			expected:    true,
+		},
+		"greater than prefixed": {
+			haveVersion: "v1.18.3",
+			wantVersion: "v1.18.0",
+			expected:    true,
+		},
+		"greater than postfixed": {
+			haveVersion: "1.18.3-gke.301",
+			wantVersion: "1.18.0#tag",
+			expected:    true,
+		},
+	}
+
+	for name, test := range tests {
+		tt := test
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := IsSupported(tt.haveVersion, tt.wantVersion)
+
+			if err != nil {
+				t.Errorf("error not allowed: %s", err.Error())
+			}
+
+			if tt.expected != actual {
+				t.Errorf("supported value doesn't match: %t != %t", tt.expected, actual)
 			}
 		})
 	}
