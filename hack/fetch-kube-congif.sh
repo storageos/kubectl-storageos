196d438448289ef68a450684234c0e0bd5df81e5
Richard Kovacs <kovacsricsi@gmail.com>
2021-11-24 11:02:55 +0100

    New github action to deploy to preprod (#98)

--- hack/fetch-kube-congif.sh
+++ hack/fetch-kube-congif.sh
@@ -6,7 +6,8 @@
 : ${KEY_FILE?= required}
 : ${PROJECT?= required}
 : ${ZONE?= required}
+: ${CLUSTER?= required}
 
 gcloud auth activate-service-account --key-file="$KEY_FILE"
 gcloud config set project $PROJECT
-gcloud --quiet container clusters get-credentials eng --zone $ZONE
+gcloud --quiet container clusters get-credentials $CLUSTER --zone $ZONE
