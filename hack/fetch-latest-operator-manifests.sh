3fa84bcff3fe8d0a20303893e8019655b2ced144
Richard Kovacs <kovacsricsi@gmail.com>
2022-01-11 19:28:45 +0100

    Fix things

--- hack/fetch-latest-operator-manifests.sh
+++ hack/fetch-latest-operator-manifests.sh
@@ -7,17 +7,19 @@
 
 IMAGE="nixery.dev/shell/jq/git/curl/kustomize/gnutar/gzip"
 
-VERSION="$(docker run --rm $IMAGE bash -c '
-curl -s https://api.github.com/repos/storageos/operator/releases |
-jq '.[]|select(.draft==false)' |
-jq -r .tag_name |
-head -1'
-)"
+if [[ -z "$VERSION" ]]; then
+    VERSION=$(docker run --rm $IMAGE bash -c "
+        curl -s https://api.github.com/repos/storageos/operator/releases |
+        jq '.[]|select(.draft==false)' |
+        jq -r .tag_name |
+        head -1
+    ")
+fi
 
-docker run --rm -v $OPERATOR_DIR:/output $IMAGE bash -c '
-git clone https://github.com/storageos/operator.git &&
-cd operator &&
-git reset --hard $VERSION &>/dev/null &&
-(cd config/manager ; kustomize edit set image controller=storageos/operator:$VERSION) &&
-(cd config ; tar -czvf /output/storageos-operator-manifests.tar.gz .)
-'
+docker run --rm -v $OPERATOR_DIR:/output $IMAGE bash -c "
+    git clone https://github.com/storageos/operator.git &&
+    cd operator &&
+    git reset --hard $VERSION &>/dev/null &&
+    (cd config/manager ; kustomize edit set image controller=storageos/operator:$VERSION) &&
+    (cd config ; tar -czvf /output/storageos-operator-manifests.tar.gz .)
+"
